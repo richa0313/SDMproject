da8f20737b0430a8a43e02902e1e801e8548c4b6
Swapnil Awasthi <swapniliec@gmail.com>
2017-05-03 18:06:42 -0400

    workdatset created

--- oil_stock_prediction.R
+++ oil_stock_prediction.R
@@ -1,6 +1,6 @@
 setwd("F:/Study/SDM")
 
-#reading file and setting the junks values to NAs
+#reading file and setting the junks values to NA
 jetfuel <- read.csv(file = 'jetfuelprices.csv', skip = 12, header = T, as.is = T, na.strings = "#N/A")
 delta <- read.csv(file = 'Deltastocks.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
 sp500 <- read.csv(file = 'Proj-SP500.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
@@ -12,7 +12,7 @@ colSums(is.na(delta))
 colSums(is.na(sp500))
 colSums(is.na(brentoil))
 
-#setting up the dates format and extracting/renaming required columns
+#setting up the dates format and extracting required columns
 sp500$Date <- as.Date(sp500$Date, '%Y-%m-%d')
 sp500Adj <- sp500[,c('Date','Adj.Close')]
 colnames(sp500Adj)[2] <- 'sp500Adj.Close'
@@ -39,12 +39,50 @@ finaldata <- sqldf('SELECT *
                                  INNER JOIN brentoil ON (sp500Adj.Date = brentoil.DATE)
                                  INNER JOIN jetfuel ON (sp500Adj.Date = jetfuel.DATE)')
 
-#removing redundant date columns 3, 5 and 7 one by one
+#removing redundant date columns 3, 5 and 7
 finaldata[3] <- NULL
 #write.csv(finaldata, "FinalData.csv")
 #plot(finaldata$sp500Adj.Close, finaldata$DeltaAdj.Close)
 
+# compute price change for snp500 data
+snp_pc1 = c(); snp_pc2= c(); snp_pc3 = c(); snp_pc4 = c(); snp_pc5 = c();
+for (i in 7:nrow(finaldata))
+  {
+  snp_pc1[i] <- (finaldata$sp500Adj.Close[i - 1] - finaldata$sp500Adj.Close[i - 2])/finaldata$sp500Adj.Close[i - 2]
+  snp_pc2[i] <- (finaldata$sp500Adj.Close[i - 2] - finaldata$sp500Adj.Close[i - 3])/finaldata$sp500Adj.Close[i - 3]
+  snp_pc3[i] <- (finaldata$sp500Adj.Close[i - 3] - finaldata$sp500Adj.Close[i - 4])/(finaldata$sp500Adj.Close[i - 4])
+  snp_pc4[i] <- (finaldata$sp500Adj.Close[i - 4] - finaldata$sp500Adj.Close[i - 5])/(finaldata$sp500Adj.Close[i - 5])
+  snp_pc5[i] <- (finaldata$sp500Adj.Close[i - 5] - finaldata$sp500Adj.Close[i - 6])/(finaldata$sp500Adj.Close[i - 6])
+  }
 
+# compute price change for brent oil
+brent_pc1 = c(); brent_pc2= c(); brent_pc3 = c(); brent_pc4 = c(); brent_pc5 = c();
+for (i in 7:nrow(finaldata))
+{
+  brent_pc1[i] <- (finaldata$BrentOilValue[i - 1] - finaldata$BrentOilValue[i - 2])/(finaldata$BrentOilValue[i - 2])
+  brent_pc2[i] <- (finaldata$BrentOilValue[i - 2] - finaldata$BrentOilValue[i - 3])/(finaldata$BrentOilValue[i - 3])
+  brent_pc3[i] <- (finaldata$BrentOilValue[i - 3] - finaldata$BrentOilValue[i - 4])/(finaldata$BrentOilValue[i - 4])
+  brent_pc4[i] <- (finaldata$BrentOilValue[i - 4] - finaldata$BrentOilValue[i - 5])/(finaldata$BrentOilValue[i - 5])
+  brent_pc5[i] <- (finaldata$BrentOilValue[i - 5] - finaldata$BrentOilValue[i - 6])/(finaldata$BrentOilValue[i - 6])
+}
+
+
+# compute price change for jet fuel oil
+jetfuel_pc1 = c(); jetfuel_pc2= c(); jetfuel_pc3 = c(); jetfuel_pc4 = c(); jetfuel_pc5 = c();
+for (i in 7:nrow(finaldata))
+{
+  jetfuel_pc1[i] <- (finaldata$JetFuelValue[i - 1] - finaldata$JetFuelValue[i - 2])/(finaldata$JetFuelValue[i - 2])
+  jetfuel_pc2[i] <- (finaldata$JetFuelValue[i - 2] - finaldata$JetFuelValue[i - 3])/(finaldata$JetFuelValue[i - 3])
+  jetfuel_pc3[i] <- (finaldata$JetFuelValue[i - 3] - finaldata$JetFuelValue[i - 4])/(finaldata$JetFuelValue[i - 4])
+  jetfuel_pc4[i] <- (finaldata$JetFuelValue[i - 4] - finaldata$JetFuelValue[i - 5])/(finaldata$JetFuelValue[i - 5])
+  jetfuel_pc5[i] <- (finaldata$JetFuelValue[i - 5] - finaldata$JetFuelValue[i - 6])/(finaldata$JetFuelValue[i - 6])
+}
+
+
+#created dataset
+workdata <- data.frame(snp_pc1, snp_pc2, snp_pc3, snp_pc4, snp_pc5, brent_pc1, brent_pc2, brent_pc3, brent_pc4, brent_pc5, jetfuel_pc1, jetfuel_pc2, jetfuel_pc3, jetfuel_pc4, jetfuel_pc5 )
+#removing empty rows 
+workdata <- workdata[7:nrow(workdata),]
 
 
 
