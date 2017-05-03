c5da45cc745986c3ad4a16f718763d9842366f54
Swapnil Awasthi <swapniliec@gmail.com>
2017-05-03 17:37:02 -0400

    updated code

--- oil_stock_prediction.R
+++ oil_stock_prediction.R
@@ -1,28 +1,69 @@
-library(sqldf)
-library(mice)
+setwd("F:/Study/SDM")
 
-setwd('/Users/birupakhya/Documents/Projects/SDMproject/')
-delta = read.csv('Deltastocks.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
-jetfuelprices = read.csv('jetfuelprices.csv', skip = 12, header = T, as.is = T, na.strings = "#N/A")
-brentprices = read.csv('DCOILBRENTEU.csv',skip = 0, header = T, as.is = T, na.strings = ".")
+#reading file and setting the junks values to NAs
+jetfuel <- read.csv(file = 'jetfuelprices.csv', skip = 12, header = T, as.is = T, na.strings = "#N/A")
+delta <- read.csv(file = 'Deltastocks.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
+sp500 <- read.csv(file = 'Proj-SP500.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
+brentoil <- read.csv(file = 'brentoilprices.csv', skip = 12, header = T, as.is = T, na.strings = "#N/A")
 
-# Transform variables into correct types
-delta$Date = as.Date(delta$Date, "%Y-%m-%d")
-jetfuelprices$DATE = as.Date(jetfuelprices$DATE, "%Y-%m-%d")
-brentprices$DATE = as.Date(brentprices$DATE, "%Y-%m-%d")
-
-# check missing values
+#checking the NA values
+colSums(is.na(jetfuel))
 colSums(is.na(delta))
-colSums(is.na(jetfuelprices))
-colSums(is.na(brentprices))
+colSums(is.na(sp500))
+colSums(is.na(brentoil))
 
-# check if there is a pattern in the missing data
-md.pattern(jetfuelprices)
-md.pattern(brentprices)
+#setting up the dates format and extracting/renaming required columns
+sp500$Date <- as.Date(sp500$Date, '%Y-%m-%d')
+sp500Adj <- sp500[,c('Date','Adj.Close')]
+colnames(sp500Adj)[2] <- 'sp500Adj.Close'
 
-# delete rows with na values
-jetfuelprices <- na.omit(jetfuelprices)
-brentprices <- na.omit(brentprices)
+delta$Date <- as.Date(delta$Date, '%Y-%m-%d')
+deltaAdj <- delta[,c('Date','Adj.Close')]
+colnames(deltaAdj)[2] <- 'DeltaAdj.Close'
+
+jetfuel$DATE <- as.Date(jetfuel$DATE, '%Y-%m-%d')
+colnames(jetfuel)[2] <- 'JetFuelValue'
+brentoil$DATE <- as.Date(brentoil$DATE, '%Y-%m-%d')
+colnames(brentoil)[2] <- 'BrentOilValue'
+
+#converting the NAs to mean values of the column - Imputation
+jetfuel[is.na(jetfuel[,2]), 2] <- mean(jetfuel[,2], na.rm = TRUE)
+brentoil[is.na(jetfuel[,2]), 2] <- mean(brentoil[,2], na.rm = TRUE)
+
+#install.packages('sqldf')
+library('sqldf')
+
+finaldata <- sqldf('SELECT *
+                   FROM sp500Adj 
+                                 INNER JOIN deltaAdj ON (sp500Adj.Date = deltaAdj.Date)
+                                 INNER JOIN brentoil ON (sp500Adj.Date = brentoil.DATE)
+                                 INNER JOIN jetfuel ON (sp500Adj.Date = jetfuel.DATE)')
+
+#removing redundant date columns 3, 5 and 7 one by one
+finaldata[3] <- NULL
+#write.csv(finaldata, "FinalData.csv")
+#plot(finaldata$sp500Adj.Close, finaldata$DeltaAdj.Close)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
