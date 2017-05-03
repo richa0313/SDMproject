8b2f49b128bbcfbee31c8a9a8156c50eef7b2c5b
birupakhya <birupakhyad@mail.usf.edu>
2017-05-03 16:09:28 -0400

    edited initial code

--- oil_stock_prediction.R
+++ oil_stock_prediction.R
@@ -1,8 +1,9 @@
 library(sqldf)
 
-delta = read.csv('/Users/birupakhya/Documents/Projects/SDMproject/Deltastocks.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
-jetfuelprices = read.csv('/Users/birupakhya/Documents/Projects/SDMproject/jetfuelprices.csv', skip = 12, header = T, as.is = T, na.strings = "#N/A")
-brentprices = read.csv('/Users/birupakhya/Documents/Projects/SDMproject/DCOILBRENTEU.csv',skip = 0, header = T, as.is = T, na.strings = ".")
+setwd('/Users/birupakhya/Documents/Projects/SDMproject/')
+delta = read.csv('Deltastocks.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
+jetfuelprices = read.csv('jetfuelprices.csv', skip = 12, header = T, as.is = T, na.strings = "#N/A")
+brentprices = read.csv('DCOILBRENTEU.csv',skip = 0, header = T, as.is = T, na.strings = ".")
 
 #check missing values
 colSums(is.na(delta))
