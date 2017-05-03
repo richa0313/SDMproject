588fd33803df24e2211d4dd3eeeeafc7effb6d08
birupakhya <birupakhyad@mail.usf.edu>
2017-05-03 15:58:26 -0400

    modified logic to identify NA

--- oil_stock_prediction.R
+++ oil_stock_prediction.R
@@ -1,7 +1,7 @@
 library(sqldf)
 
 delta = read.csv('/Users/birupakhya/Documents/Projects/SDMproject/Deltastocks.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
-jetfuelprices = read.csv('/Users/birupakhya/Documents/Projects/SDMproject/jetfuelprices.csv', skip = 12, header = T, as.is = T, na.strings = "")
+jetfuelprices = read.csv('/Users/birupakhya/Documents/Projects/SDMproject/jetfuelprices.csv', skip = 12, header = T, as.is = T, na.strings = "#N/A")
 brentprices = read.csv('/Users/birupakhya/Documents/Projects/SDMproject/DCOILBRENTEU.csv',skip = 0, header = T, as.is = T, na.strings = ".")
 
 #check missing values
