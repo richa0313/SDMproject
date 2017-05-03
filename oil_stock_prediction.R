df65ff09d32bb20b40f45b17b0bd5ee7a2e93608
birupakhya <birupakhyad@mail.usf.edu>
2017-05-03 15:48:21 -0400

    Updated file read and missing value check

---
+++ oil_stock_prediction.R
@@ -0,0 +1,12 @@
+library(sqldf)
+
+delta = read.csv('/Users/birupakhya/Documents/Projects/SDMproject/Deltastocks.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
+jetfuelprices = read.csv('/Users/birupakhya/Documents/Projects/SDMproject/jetfuelprices.csv', skip = 12, header = T, as.is = T, na.strings = "")
+brentprices = read.csv('/Users/birupakhya/Documents/Projects/SDMproject/DCOILBRENTEU.csv',skip = 0, header = T, as.is = T, na.strings = ".")
+
+#check missing values
+colSums(is.na(delta))
+colSums(is.na(jetfuelprices))
+colSums(is.na(brentprices))
+
+
