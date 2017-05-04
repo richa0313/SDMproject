3702f4a4e3a208ae46f6cb20b4ce1bb3dc53cde3
Swapnil Awasthi <swapniliec@gmail.com>
2017-05-04 00:07:53 -0400

    NAs correction

--- oil_stock_prediction.R
+++ oil_stock_prediction.R
@@ -4,7 +4,7 @@ setwd("F:/Study/SDM")
 jetfuel <- read.csv(file = 'jetfuelprices.csv', skip = 12, header = T, as.is = T, na.strings = "#N/A")
 delta <- read.csv(file = 'Deltastocks.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
 sp500 <- read.csv(file = 'Proj-SP500.csv', skip = 0, header = T, as.is = T, na.strings = "NA")
-brentoil <- read.csv(file = 'brentoilprices.csv', skip = 12, header = T, as.is = T, na.strings = "#N/A")
+brentoil <- read.csv(file = 'brentoilprices.csv', skip = 12, header = T, as.is = T, na.strings = c("NA", "#N/A" ))
 
 #checking the NA values
 colSums(is.na(jetfuel))
