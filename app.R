9bbd7c0cd710d75f60c9292f72d2a1d4f3fd975e
swapnilawasthi <swapniliec@gmail.com>
2017-05-05 18:03:27 -0400

    with diff model summary

--- app.R
+++ app.R
@@ -59,12 +59,16 @@ body <- dashboardBody(theme = "bootstrap.css",
                                 fluidRow(
                                   box(solidHeader = TRUE, selectInput("dataset", "Choose a dataset:", choices = c("Brent Oil", "Jet Fuel", "delta", "S$P500 Index", "Workdataset")),
                                       numericInput("obs", "Number of observations to view:", 20),
-                                      verbatimTextOutput("summary")),
+                                      verbatimTextOutput(outputId = "summary")),
                                   box(title = "Table view", div(style = 'overflow-x: scroll', tableOutput("view")))
                                 )
                         ),
                         tabItem(tabName = "Models",
-                                box(title = "Models", selectInput("Models", "Choose a Model:", choices = c("NaiveBayes", "Linear Regression", "Logistic Regression", "R-Part", "GBM")))
+                                fluidRow(
+                                  box(solidHeader = TRUE, selectInput("selmodel", "Choose a Model:", choices = c("NaiveBayes", "Linear Regression", "Logistic Regression", "R-Part", "GBM")),
+                                      plotOutput(outputId = "summaryModels")),
+                                  box(title = "Plot View", solidHeader = TRUE,  verbatimTextOutput(outputId = "predY"))
+                                )   
                         )
                       )
 )
@@ -80,6 +84,19 @@ server <- shinyServer(function(input, output){
     dataset <- datasetInput()
     summary(dataset)
   })  
+  
+  modelsInput <- reactive({    
+    switch(input$selmodel, "NaiveBayes" = nb , "Linear Regression"= linear.reg, "Logistic Regression" = lr.delta.01, "R-Part" = model.rpt, "GBM" = model.gbm)
+  })
+  
+  output$predY <- renderPrint({
+    y <- modelsInput()
+    print(y)
+  }) 
+  
+  output$summaryModels <- renderPlot({
+    plot(linear.predict, test$delta_pc1) 
+  })
   # Show the first "n" observations
   output$view <- renderTable({head(datasetInput(), n = input$obs)},include.rownames=TRUE, digits=6)
 })
